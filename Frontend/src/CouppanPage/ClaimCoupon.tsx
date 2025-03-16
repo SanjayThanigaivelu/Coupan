b93dd3bf0146ab3fe13e8e37501df88db0425a26
SanjayThanigaivelu <sanjaymuthulakshmi@gmail.com>
2025-03-16 22:23:14 +0530

    React change

--- Frontend/src/CouppanPage/ClaimCoupon.tsx
+++ Frontend/src/CouppanPage/ClaimCoupon.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import {useState} from 'react';
 import axios from 'axios';
 import './ClaimCoupon.css';
 import loginBackground2 from  '../../assets/loginBackground2.jpg'
